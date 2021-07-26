c4beeb7ea248f6fca3e10519c381fd75a05d861b
Are1PM <arwanpriantomangidi@gmail.com>
2021-07-27 00:56:54 +0800

    Add Column with Text in body Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ class MyApp extends StatelessWidget {
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: Text(
+                    'Bokori Island',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                )
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
