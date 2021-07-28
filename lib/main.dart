4a7d2ae028ad6e594c08656b57ad211ff679710f
Are1PM <arwanpriantomangidi@gmail.com>
2021-07-28 08:28:15 +0800

    move class DetailScreen to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_sultra/detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,72 +12,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(),
       home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(  // Agar diberikan padding yg secara otomatis menyesuaikan perangkat yang digunakan
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-
-            Container(
-                margin: EdgeInsets.only(top: 16.0),
-                child: Text(
-                    'Bokori Island',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                )
-            ),
-
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0), // membuat kotak untuk memberi jarak
-                      Text('Open Everydary'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8.0), // membuat kotak untuk memberi jarak
-                      Text('09:00 - 20:00'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8.0), // membuat kotak untuk memberi jarak
-                      Text('Rp. 25.000'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Pulau Bokori adalah tempat yang memiliki keindahan alam memukau di Sulawesi Tenggara. Pasti yang melewatkan keindahan tempat ini akan menyesal. Agar bisa sampai pulau satu ini harus menggunakan perahu terlebih dahulu. Setelah selesai menyebrang pasti mata akan dimanjakan dengan view yang luar biasa.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-
-          ],
-        ),
-      ),
     );
   }
 }
